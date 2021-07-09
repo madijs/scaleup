a2398d595ac12f236715903b64372d50fbd1a017
madi bikamalov <m.bikamalov@kit.systems>
2021-07-10 03:40:47 +0600

    as

--- src/components/FaqComponents/Faq.js
+++ src/components/FaqComponents/Faq.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import styles from '../../assets/styles/FaqStyles/Faq.module.scss'
 import {useDispatch, useSelector} from "react-redux";
@@ -6,6 +6,7 @@ import {getFaqListAction} from "../../redux/actions/getFaqListAction";
 import {useHistory} from 'react-router-dom';
 import FaqList from "./FaqList";
 import {useLocation} from "react-router-dom"
+import {getJustFaqsAction} from "../../redux/actions/getJustFaqsAction";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -22,11 +23,26 @@ const Faq = ({faqData=[]}) => {
     const history = useHistory();
     // const {faqData} = useSelector(state => state.MainPage);
     const dispatch = useDispatch();
+    const [faq,setFaqData] = useState(null);
     const classes = useStyles();
 
     // useEffect(()=>{
     //     dispatch(getFaqListAction());
     // },[]);
+
+    useEffect(()=>{
+        if (location.pathname.includes('/form/strategy') || location.pathname.includes('/form/financial') || location.pathname.includes('/form/marketing') || location.pathname.includes('/form/legal')){
+            const response2 = dispatch(getJustFaqsAction());
+            response2.then(res=>{
+                for (let i=0;i<res.data.length;i++){
+                    if (res.data[i].id == 2){
+                        setFaqData(res.data[i].faqs)
+                    }
+                }
+                console.log(res.data);
+            })
+        }
+    },[]);
 
     return(
         <>
@@ -40,7 +56,7 @@ const Faq = ({faqData=[]}) => {
                            {/*Learn from a growing library of 1,982 websites and 3,829 component examples. Easily filterable to find the inspiration you need, quickly.*/}
                        </div>
                        {faqData && (
-                           <FaqList faqDatas={faqData} location={location}/>
+                           <FaqList faqDatas={faq ? faq : faqData} location={location}/>
                        )}
                        <div onClick={()=>history.push('/faq')} className={styles.allQuestions}>
                            Все вопросы-ответы
